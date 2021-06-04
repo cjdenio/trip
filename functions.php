dea8968f45e161f19dc31b649ae6aafa9c21c1c6
Caleb Denio <cjdenio44@gmail.com>
2021-06-04 16:39:01 -0400

    Fix site title

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php
 
 add_theme_support('post-thumbnails');
+add_theme_support('title-tag');
 
 function enqueue_styles()
 {
--- page.php
+++ page.php
@@ -3,7 +3,6 @@
 
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
-    <title><?php wp_title('|', true, 'right'); ?></title>
     <link rel="stylesheet" href="<?php echo esc_url(get_stylesheet_uri()); ?>" type="text/css" />
     <?php wp_head(); ?>
 </head>

969b0319bfe45b1d3e968324e306607ba8616249
Caleb Denio <cjdenio44@gmail.com>
2021-06-21 11:39:30 -0400

    ✨ Slack updates

--- functions.php
+++ functions.php
@@ -43,8 +43,60 @@ function comment_form_fields(array $fields): array {
     
     return $fields;
 }
+
+// Send posts to Slack (stolen in part from https://davidwalsh.name/wordpress-publish-post-hook)
+function send_new_post(string $new_status, string $old_status, WP_Post $post) {
+  if('publish' === $new_status && 'publish' !== $old_status && $post->post_type === 'post') {
+    // Do something!
+    $thumbnail = get_the_post_thumbnail_url($post, "thumbnail");
+    $permalink = get_permalink($post);
+
+    $env = json_decode(file_get_contents(__DIR__ . "/env.json"));
+
+    foreach ($env->slack as $webhook ) {
+        $blocks = [
+            [
+                "type" => "section",
+                "text" => [
+                    "type" => "mrkdwn",
+                    "text" => "*" . $post->post_title . "*"
+                ]
+            ],
+            [
+                "type" => "section",
+                "text" => [
+                    "type" => "mrkdwn",
+                    "text" => "<" . $permalink . "|view on trip.calebden.io>"
+                ]
+            ],
+            [
+                "type" => "image",
+                "image_url" => $thumbnail,
+                "alt_text" => "a cool image"
+            ]
+        ];
+
+        $body = json_encode(array(
+            "blocks" => $blocks,
+            "username" => $webhook->username,
+            "icon_url" => $webhook->icon_url
+        ));
+
+        $resp = wp_remote_post($webhook->webhook, array(
+            "body" => $body,
+            "headers" => array(
+                "Content-Type" => "application/json"
+            )
+        ));
+
+        file_put_contents(__DIR__ . "/log", print_r($resp, true));
+    }
+  }
+}
  
 add_filter('comment_form_fields', 'comment_form_fields');
 
 add_action("wp_enqueue_scripts", "enqueue_styles");
 add_action("init", "register_menus");
+
+add_action('transition_post_status', 'send_new_post', 10, 3);
